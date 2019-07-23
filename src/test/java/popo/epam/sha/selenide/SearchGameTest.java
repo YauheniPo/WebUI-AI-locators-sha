49c291c58615d13fc3fd7bf6a8cc619d3938074b
YauheniPo <ip.popovich@mail.ru>
2019-07-23 22:06:04 +0300

    Added SHA lib WebUI for test framework

--- src/test/java/popo/epam/sha/selenide/SearchGameTest.java
+++ src/test/java/popo/epam/sha/selenide/SearchGameTest.java
@@ -2,23 +2,31 @@ package popo.epam.sha.selenide;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import popo.epam.sha.selenide.pages.SteamMainPage;
 import popo.epam.sha.selenide.pages.SteamSearchPage;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
 public class SearchGameTest extends BaseTest {
+
+    private String game;
+
+    @BeforeClass
+    @Parameters("game")
+    public void beforeClass(@Optional(value = "Dota") String game) {
+        this.game = game;
+    }
 
     @Test
     public void testSearchGame() {
-        final String dotaGame = "Dota";
-
         SteamMainPage steamMainPage = new SteamMainPage();
-        SteamSearchPage steamSearchPage = steamMainPage.search(dotaGame);
+        SteamSearchPage steamSearchPage = steamMainPage.search(game);
         ElementsCollection results = steamSearchPage.getResults();
-        assertThat(String.format("The first searching result has not text '%s'", dotaGame),
-                results.first().has(Condition.text(dotaGame)));
+        assertThat(String.format("The first searching result has not text '%s'", game),
+                results.first().has(Condition.text(game)));
     }
 }
